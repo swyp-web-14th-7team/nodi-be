2811ff131c26d5035c9bfed12035f8a8725f1df6
hyeongu01 <cdy5212@gmail.com>
2026-06-24 12:34:29 +0900

    feat: auth device_id 쿠키 추가

--- src/feature/auth/auth.controller.ts
+++ src/feature/auth/auth.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AuthService } from '@/feature/auth/auth.service';
+import type { CookieOptions, Request, Response } from 'express';
+import { randomUUID } from 'crypto';
+import { ConfigService } from '@nestjs/config';
+
+const DEVICE_ID_COOKIE_KEY: string = 'device_id';
+const DEVICE_ID_MAX_AGE_MS: number = 400 * 24 * 60 * 60 * 1000;
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  private deviceIdCookieOptions(): CookieOptions {
+    return {
+      httpOnly: true,
+      secure: this.configService.get<string>('NODE_ENV') === 'prod',
+      sameSite: 'none', // 프론트 != 백엔드 인 경우 none, 같은 경우 lax
+      path: '/',
+      maxAge: DEVICE_ID_MAX_AGE_MS,
+    };
+  }
 
   @Get('google')
-  async getGoogleLoginUrl(): Promise<{ url: string }> {
-    // TODO: device_id 쿠키 저장
+  async getGoogleLoginUrl(
+    @Req() req: Request,
+    @Res() res: Response,
+  ): Promise<{ url: string }> {
+    const deviceId: string =
+      (req.cookies?.[DEVICE_ID_COOKIE_KEY] as string) ?? randomUUID();
+
+    res.cookie(DEVICE_ID_COOKIE_KEY, deviceId, this.deviceIdCookieOptions());
     const url: string = await this.authService.getGoogleAuthUrl();
     return { url };
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { TransformInterceptor } from '@/common/interceptor/transform.interceptor';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
