c95de955f8ebc78ec131c473d4e8bf6fa6bd4867
hyeongu01 <cdy5212@gmail.com>
2026-07-23 12:39:18 +0900

    fix: users 응답 형식 최적화

--- src/module/users/type/user-response.type.ts
+++ src/module/users/type/user-response.type.ts
@@ -1,6 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { User } from '@/prisma/client';
 import { FormattedDate } from '@/common/type/formatted-date.type';
+import { UserWithLastLogin } from '@/module/users/users.type';
 
 export class UserResponse {
   @ApiProperty({ description: '유저 ID' })
@@ -21,7 +22,21 @@ export class UserResponse {
   @ApiProperty()
   updatedAt: FormattedDate;
 
-  static fromUser(item: User): UserResponse {
+  @ApiPropertyOptional({ description: '권한 (0: 일반 유저, 1: ADMIN)' })
+  role?: number;
+
+  @ApiPropertyOptional({
+    nullable: true,
+    description: '탈퇴 시각 (미탈퇴 시 null)',
+  })
+  deletedAt?: FormattedDate | null;
+
+  @ApiPropertyOptional({
+    description: '마지막 로그인 시각 (가장 최근 리프레시 토큰 갱신 시각)',
+  })
+  lastLoginAt?: FormattedDate;
+
+  static fromUser(item: User | UserWithLastLogin): UserResponse {
     return {
       id: item.id,
       name: item.name,
@@ -29,6 +44,15 @@ export class UserResponse {
       email: item.email,
       createdAt: FormattedDate.fromDate(item.createdAt),
       updatedAt: FormattedDate.fromDate(item.updatedAt),
+      ...('refreshTokens' in item && {
+        role: item.role,
+        deletedAt: item.deletedAt
+          ? FormattedDate.fromDate(item.deletedAt)
+          : null,
+        ...(item.refreshTokens[0] && {
+          lastLoginAt: FormattedDate.fromDate(item.refreshTokens[0].updatedAt),
+        }),
+      }),
     };
   }
 }
